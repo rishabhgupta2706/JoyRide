8a55feb291df50f7b6090cf0eb9e95e7a3ac5695
Rishabh <guptarishabh2706@gmail.com>
2026-08-07 20:27:41 +0530

    Add JWT authentication middleware and protected routes

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,44 @@
 const jwt = require("jsonwebtoken");
-
+const User = require("../models/User");
 const protect = async (req, res, next) => {
     try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({
+                success: false,
+                message: "Access denied. No token provided."
+            });
+        }
+
+        // Extract token
+        const token = authHeader.split(" ")[1];
+
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Attach user data to request
+       const user = await User.findById(decoded.id).select("-password");
+
+if (!user) {
+    return res.status(401).json({
+        success: false,
+        message: "User not found"
+    });
+}
+
+req.user = user;
+
+        // Continue to the next middleware/controller
+        next();
 
     } catch (error) {
+        console.log(error);
 
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized"
+        });
     }
 };
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const protect = require("../middleware/authMiddleware");
 
 const { registerUser, loginUser } = require("../controllers/userControllers");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", protect, (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Profile fetched successfully",
+        user: req.user
+    });
+});
 
 module.exports = router;
