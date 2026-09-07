0f1b66e4118de250c153972cc9048f25f6da1caf
Rishabh <guptarishabh2706@gmail.com>
2026-09-07 19:52:35 +0530

    making deployement compatible

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URL);
 
         console.log("MongoDB connected successfully");
     } catch (error) {
